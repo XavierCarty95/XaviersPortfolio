c4b30424c03e9d09c1dd8a609ea2270d9e2b6ced
Xavier Carty <xaviercarty@gmail.com>
2020-07-12 09:53:31 -0400

    Updates

---
+++ precache-manifest.1245b945cfb391180ac06d040fbf6367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3495c7b1b2a56c64cf9ad2d3da009c3",
+    "url": "/XaviersPortfolio/index.html"
+  },
+  {
+    "revision": "0d3c3a0bc1ec91104bac",
+    "url": "/XaviersPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b580882fa9f7e1d961dd",
+    "url": "/XaviersPortfolio/static/css/main.c0d6cdd3.chunk.css"
+  },
+  {
+    "revision": "0d3c3a0bc1ec91104bac",
+    "url": "/XaviersPortfolio/static/js/2.63ba8df6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/XaviersPortfolio/static/js/2.63ba8df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b580882fa9f7e1d961dd",
+    "url": "/XaviersPortfolio/static/js/main.c480b86d.chunk.js"
+  },
+  {
+    "revision": "bf7a88a573068f5c19a5",
+    "url": "/XaviersPortfolio/static/js/runtime-main.eb0af603.js"
+  }
+]);
